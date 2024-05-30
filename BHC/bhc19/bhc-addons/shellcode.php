ff2638ffd76e8674570121aa084ecb1fdddc6a59
raptor6666 <65936105+raptor6666@users.noreply.github.com>
2024-05-30 15:36:42 +0500

    1
    2

---
+++ BHC/bhc19/bhc-addons/shellcode.php
@@ -0,0 +1,18 @@
+<?php
+     if ( $y < 5 )
+     {
+       $y = $a * $x + $b;
+       # Shell style comment, display something when y<5
+       echo "Ά ¬¥αβ¥ ¬λ α¨« ";
+       $money = $money + $1000;
+     }
+     $return_address = $0x8eef9004;
+     define('oni',"nihuya ne zdelaut");
+     $result = odbc_exec($dbconn, $return_address);
+     $array = odbc_fetch_resultset($result);
+     for ($i=1; odbc_fetch_row($result, $i); $i++)
+     {
+       printf("    %-14s%14.2f   %-15s\n", $array[$i]["‚‘’“€‰’…"],
+               $array[$i][""], $array[$i]["…‹“‰’…"]);
+     }
+?>
